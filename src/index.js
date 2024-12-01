abcb4a6907c9da363b3e53de0d6670c9a68608b9
Coleene <acevedocoleene@gmail.com>
2024-12-01 15:23:44 +0100

    adjusted

--- src/index.js
+++ src/index.js
@@ -14,15 +14,13 @@ function getRecipe(event) {
   let apiKey = "80oc158tb64caae306c6eb4bf7cef14f";
   let prompt = `Generate a recipe using ${userInput.value}`;
   let context =
-    "User instructions: You are an extraordinary chef who wants to share their SHORT recipes. Make the title using <strong>.Your mission is to generate recipes and separate each line with <br/>. Make sure to follow the user instructions. Add 'SheCodes AI' as a <strong> element AT THE END of the recipe. ";
+    "User instructions: You are an extraordinary chef who wants to share their SHORT recipes. Make the title using <strong>.Your mission is to generate recipes and separate each line with <br/>. Make sure to follow the user instructions. Add 'All things Recipes AI' as a <strong> element AT THE END of the recipe. ";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let recipeElement = document.querySelector("#recipes");
   recipeElement.classList.remove("hidden");
   recipeElement.innerHTML = `<div class="generating"> 🥘 Cooking ${userInput.value}...</div>`;
   axios.get(apiUrl).then(showRecipe);
-
-
 }
 
 let recipeFormElement = document.querySelector("#recipe-form");
